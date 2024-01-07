e9c3fd7509c20a5eff61f8d616ede1d45066de1e
pkdyn <puneetsk21@gmail.com>
2024-01-07 15:19:03 +0530

    version updates

--- mvr.py
+++ mvr.py
@@ -15,14 +15,14 @@ model = load_model('xception_v1_03_0.983.h5')
 #model = make_model(learning_rate=0.001, droprate=0.6, size=25)
 #model.fit(train_ds, epochs=30, validation_data=val_ds, callbacks=[checkpoint])
 
-uploaded_file = st.file_uploader("Upload Book Cover", type="jpg")
+uploaded_file = st.file_uploader("Upload Book Cover", type=["jpg", "png"])
 if uploaded_file is not None:
     uimg = Image.open(uploaded_file)
     uimg = uimg.save('379f996e73289292a42a9c2bc322a5d4.jpg')
     #st.image(uimg, caption='Uploaded Image', use_column_width=True)
     img_res=(100, 100)
     imgs = []
-    img = imageio.imread('379f996e73289292a42a9c2bc322a5d4.jpg', pilmode = "RGB")
+    img = imageio.imread('379f996e73289292a42a9c2bc322a5d4.jpg' , pilmode = "RGB")
     img = np.array(Image.fromarray(img).resize(img_res))
     imgs.append(img)
     imgs = np.array(imgs)/127.5 - 1.
@@ -32,4 +32,6 @@ if uploaded_file is not None:
         st.write("That's an O'Reilly publication.")
     else:
         st.write("That's a Manning publication.")
+
+
 
